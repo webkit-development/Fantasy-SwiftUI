aa94289886d795dfeed64484a764b1dfd980bed9
Kevin Stradtman <webkit.development.systems@gmail.com>
2022-04-03 05:31:01 -0400

    Initial Commit

---
+++ CFB/CFBApp.swift
@@ -0,0 +1,17 @@
+//
+//  CFBApp.swift
+//  CFB
+//
+//  Created by Kevin Stradtman on 4/3/22.
+//
+
+import SwiftUI
+
+@main
+struct CFBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CFB/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CFB
+//
+//  Created by Kevin Stradtman on 4/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
